df612950ac5d3e6e6be89dea4c2a68d2c2c170b4
Yaswanth <pyaswanth4@gmail.com>
2022-09-18 15:26:39 +0530

    .

---
+++ MVCWithLinq1/Controllers/StudentController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCWithLinq1.Models;
+
+namespace MVCWithLinq1.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDAL obj = new StudentDAL();
+        public ViewResult DisplayStudents()
+        {
+            return View(obj.GetStudents(true));
+        }
+        public ViewResult DisplayStudent(int Sid)
+        {
+            return View(obj.GetStudent(Sid,true));
+        }
+        [HttpGet]
+        public ViewResult AddStudent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public RedirectToRouteResult AddStudent(Student student)
+        {
+            obj.AddStudent(student);
+            return RedirectToAction("DisplayStudents");
+        }
+    }
+}
---
+++ MVCWithLinq1/Models/StudentDAL.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCWithLinq1.Models
+{
+    public class StudentDAL
+    {
+        MVCDBDataContext context = new MVCDBDataContext();
+        public List<Student> GetStudents(bool? Status)
+        {
+            List<Student> students;
+            try
+            {
+                if (Status != null)
+                {
+                    students = (from s in context.Students where s.Status == Status select s).ToList();
+                }
+                else
+                {
+                    students = (from s in context.Students select s).ToList();
+                }
+                return students;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public Student GetStudent(int Sid, bool? Status)
+        {
+            Student student;
+            try
+            {
+                if (Status == null)
+                {
+                    student = (from s in context.Students where s.Sid == Sid select s).Single();
+                }
+                else
+                {
+                    student = (from s in context.Students where s.Sid == Sid && s.Status == Status select s).Single();
+                }
+                return student;
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void AddStudent(Student student)
+        {
+            try
+            {
+                context.Students.InsertOnSubmit(student);
+                context.SubmitChanges();    
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
